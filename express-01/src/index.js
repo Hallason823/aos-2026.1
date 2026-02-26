a74b277f930421ad2cf27726b66acc25d64f5076
Hallason Matias <122497324+Hallason823@users.noreply.github.com>
2026-02-26 23:00:57 +0000

    feat: add the method get basic

--- express-01/src/index.js
+++ express-01/src/index.js
@@ -1,1 +1,14 @@
-console.log("Hello ever running Node.js project!")
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+
+const porta = process.env.PORT ?? 3000;
+
+app.get("/", (req, res) => {
+    res.send("RES: " + process.env.MESSAGE);
+});
+
+app.listen(porta, () => console.log(
+    "Example app listening on port " + porta + "!\n" + process.env.MESSAGE
+));
