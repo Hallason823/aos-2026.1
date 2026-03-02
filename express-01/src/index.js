69d45ffd4d89fb5af81119c1d91ba1b9a42894c8
Hallason Matias <122497324+Hallason823@users.noreply.github.com>
2026-03-02 22:00:21 +0000

    feat: deploy with versel

--- express-01/src/index.js
+++ express-01/src/index.js
@@ -3,12 +3,13 @@ import express from "express";
 
 const app = express();
 
-const porta = process.env.PORT ?? 3000;
-
 app.get("/", (req, res) => {
-    res.send("RES: " + process.env.MESSAGE);
+    res.send("RES: " + (process.env.MESSAGE || "Without message"));
 });
 
-app.listen(porta, () => console.log(
-    "Example app listening on port " + porta + "!\n" + process.env.MESSAGE
-));
+if (process.env.NODE_ENV !== 'production') {
+    const porta = process.env.PORT ?? 3000;
+    app.listen(porta, () => console.log(`Running in the http://localhost:${porta}`));
+}
+
+export default app;
